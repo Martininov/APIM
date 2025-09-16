970d5f84b6b3da8e295dffa9868b8e0c62f324be
Martininov <Boguiemartin@gmail.com>
2025-09-16 18:15:47 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import understat.constants as constants
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 Understat API est en ligne sur Render"
+
+@app.route("/constants")
+def get_constants():
+    return jsonify({k: getattr(constants, k) for k in dir(constants) if not k.startswith("__")})
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
